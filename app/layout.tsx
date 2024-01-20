e2f8aae7ee1b941ac55ca986680281bc4733d2a2
Jacob Bøtter <jacob@boetter.dk>
2024-01-20 12:26:56 +0100

    den taler dansk

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en">
+        <html lang="da">
             <head>
                 {/* Description no longer than 155 characters */}
                 <meta
